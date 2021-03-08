c708450c553ba0f1cdc550d858eb7e9d47fbd49f
José Estrada <joseesgutierrez12@gmail.com>
2021-03-08 17:27:35 -0600

    Hoja de Trabajo 1
    José Manuel Estrada Gutiérrez 201907078

--- HojadeTrabajo1.py
+++ HojadeTrabajo1.py
@@ -1,5 +1,19 @@
-peso_persona = float(input("Introduzca su peso en kilogramos: "))
-altura_persona = float(input("Introduzca su altura en metros: "))
-IMC = float(str(round(peso_persona/(altura_persona + altura_persona), 2)))
-print("Tu índice de masa corporal es:", IMC)
+altura = int(input("Introduzca la altura del triangulo: "))
+for i in range(altura):
+   print("*"*(i+1))
+
+
+numero = int(input("Introduzca el número deseado: "))
+for i in range (numero, -1, -1):
+   print(i, end=", ")  
+
+
+num = int(input("Introduzca el número(mayor a 2): "))
+for i in range(2, num):
+   if num % i == 0:
+      break
+if(i + 1) == num:
+   print(str(num) + " es primo.")
+else:
+   print(str(num) + " es par.")
 
